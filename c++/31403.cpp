830603acd0cfd30d4dc27b72a3cfdacd3679b4ad
minsky922 <minsky922@gmail.com>
2024-06-15 00:04:27 +0900

    baekjoon

--- c++/31403.cpp
+++ c++/31403.cpp
@@ -8,7 +8,7 @@
         //cin.tie는  cin과 cout을 묶어 입출력 속도 개선
         // endl 대신 '\n' 써야 빠름
         cin >> a >> b >> c;
-        cout << stoi(a) + stoi(b) - stoi(c) << "\n";
+        cout << stoi(a) + stoi(b) - stoi(c) << "\n"; // stoi : string to int
         cout << stoi(a+b) - stoi(c);
         return 0;
     }
