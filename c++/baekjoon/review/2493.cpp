cb9de18d49e13dab38a300332a4f103cd315db7f
minsky922 <minsky922@gmail.com>
2024-07-02 16:40:42 +0900

    탑-스택

---
+++ c++/baekjoon/review/2493.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int n, height;
+stack<pair<int, int>> s;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> height;
+    while (!s.empty()) {
+      if (height < s.top().second) {
+        cout << s.top().first << " ";
+        break;
+      }
+      s.pop();
+    }
+    if (s.empty()) {
+      cout << 0 << " ";
+    }
+    s.push({i + 1, height});
+  }
+}
