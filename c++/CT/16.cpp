1e2acea271a8032eef65b47670ef0329ad639d86
minsky922 <minsky922@gmail.com>
2024-07-17 21:42:21 +0900

    기능개발

--- c++/CT/16.cpp
+++ c++/CT/16.cpp
@@ -1,26 +1,27 @@
-#include <queue>
+#include <cmath>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-string solution(vector<string> cards1, vector<string> cards2,
-                vector<string> goal) {
-  queue<string> c1, c2, g;
-  for (const string& s : cards1) c1.push(s);
-  for (const string& s : cards2) c2.push(s);
-  for (const string& s : goal) g.push(s);
-
-  while (!g.empty()) {
-    if (!c1.empty() && c1.front() == g.front()) {
-      c1.pop();
-      g.pop();
-    } else if (!c2.empty() && c2.front() == g.front()) {
-      c2.pop();
-      g.pop();
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+  vector<int> answer;
+  int n = progresses.size();
+  vector<int> days_left(n);
+  for (int i = 0; i < n; i++) {
+    days_left[i] = ceil((100.0 - progresses[i]) / speeds[i]);
+  }
+  int count = 0;
+  int max_day = days_left[0];
+  for (int i = 0; i < n; i++) {
+    if (days_left[i] <= max_day) {
+      count++;
     } else {
-      break;
+      answer.push_back(count);
+      count = 1;
+      max_day = days_left[i];
     }
   }
-  return g.empty() ? "Yes" : "No";
+  answer.push_back(count);
+  return answer;
 }
