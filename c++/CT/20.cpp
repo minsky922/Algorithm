a077ed256bf17f4e6afe89270cefa679494bbb59
minsky922 <minsky922@gmail.com>
2024-07-23 17:04:28 +0900

    완주하지 못한 선수

---
+++ c++/CT/20.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+  unordered_map<string, int> ph;
+  for (int i = 0; i < participant.size(); i++) {
+    ph[participant[i]]++;
+  }
+  for (int i = 0; i < completion.size(); i++) {
+    ph[completion[i]]--;
+    if (ph[completion[i]] == 0) ph.erase(ph.find(completion[i]));
+  }
+  return ph.begin()->first;
+}
